b91f7f961beea8d7cc89a8c25f19069149b010ef
Adhisha <adhishagc@gmail.com>
2019-07-02 00:15:05 +0530

    swipe

--- swipe/swipe.ino
+++ swipe/swipe.ino
@@ -1,9 +1,21 @@
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print("A0 : ");
+  Serial.print(analogRead(A0));
 
+  Serial.print(", A1 : ");
+  Serial.print(analogRead(A1));
+
+  Serial.print(", A2 : ");
+  Serial.print(analogRead(A2));
+
+  Serial.println(""); 
 }
